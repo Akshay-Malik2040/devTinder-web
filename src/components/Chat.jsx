a40f313883679f85f3d8de81bed0b88f349601b7
Akshay-Malik2040 <akshaymalik2040@gmail.com>
2026-01-16 18:43:43 +0530

    Create Chat Component and set route

---
+++ src/components/Chat.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom';
+
+const Chat = () => {
+    const targetUserId = useParams();
+    const [messages, setMessages] = useState([{ text: "Hello world" }]);
+
+    return (
+        <div className='flex h-[70vh] my-3 border border-gray-600 justify-center w-1/2 mx-auto flex-col'>
+            <h1 className='border-b p-2 border-gray-500 w-full'>Chat</h1>
+            <div className='flex-1'>
+                {/* display message */}
+                {messages.map((msg, index) => {
+                    return (<div key={index}><div className="chat chat-start">
+                        <div className="chat-header">
+                            Obi-Wan Kenobi
+                            <time className="text-xs opacity-50">2 hours ago</time>
+                        </div>
+                        <div className="chat-bubble">You were the Chosen One!</div>
+                        <div className="chat-footer opacity-50">Seen</div>
+                        </div>
+                        <div className="chat chat-start">
+                        <div className="chat-header">
+                            Obi-Wan Kenobi
+                            <time className="text-xs opacity-50">2 hour ago</time>
+                        </div>
+                        <div className="chat-bubble">I loved you.</div>
+                        <div className="chat-footer opacity-50">Delivered</div>
+                        </div></div>);
+                })}
+            </div>
+
+            <div className='flex border justify-center items-center gap-2 border-gray-500'>
+                {/*For input and button */}
+                <input className='flex-1 border border-gray-500 rounded-3xl p-2 m-2' type="text" />
+                <button className='btn btn-primary rounded-3xl m-2'>Send</button>
+            </div>
+        </div>
+    )
+}
+
+export default Chat
--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { BASE_URL } from '../utils/constant'
 import { useDispatch, useSelector } from 'react-redux'
 import { addConnection } from '../utils/connectionSlice'
+import { Link } from 'react-router-dom'
 
 const Connections = () => {
     const dispatch = useDispatch();
@@ -28,10 +29,12 @@ const Connections = () => {
             <h1 className='text-center py-2 font-bold text-3xl'>Connections</h1>
             <div className='flex justify-center'>
                 {connections.map((connection) => (
-                    <div key={connection._id} className='flex justify-center w-1/2 bg-base-300 rounded my-4 mx-auto'>
-                        <div className='my-2'><div>{connection.firstName + " " + connection.lastName}</div>
+                    <div key={connection._id} className='flex justify-between w-1/2 bg-base-300 rounded my-4 mx-auto'>
+                        <div className='my-2 mx-4'><div>{connection.firstName + " " + connection.lastName}</div>
                             <div>{connection.about}</div></div>
                         <p></p>
+                        <Link to={"/chat/"+connection._id}><button className='bg-primary mx-3 my-2 p-3 rounded-3xl'>Chat</button></Link>
+                        {/* <Link to={"/user"+connection._id}><button>Chat</button></Link> */}
                     </div>
                 ))}
             </div>
